eb1722eb528401fe2b56439be09cbc1af31ef6ba
manhamirzaa <manhamirza23@gmail.com>
2026-01-24 23:26:00 -0500

    fixed something

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.config'
 
-export default defineConfig({
-  plugins: [
-    react(),
-    crx({ manifest }),
-  ],
-})
+export default defineConfig(({ command }) => ({
+  plugins: [react(), crx({ manifest })],
+  ...(command === 'serve'
+    ? { server: { host: true, port: 5173, strictPort: true, cors: true, headers: { 'Access-Control-Allow-Origin': '*' } } }
+    : {}),
+}))
